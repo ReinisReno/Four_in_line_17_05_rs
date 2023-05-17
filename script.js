a213662664051be1cc39ed0e3163cfef6dc57460
ReinisReno <reinisreno@gmail.com>
2023-05-17 21:00:10 +0300

    doneee

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const template = document.querySelector('#game_cell');
 
 for (let index = 0; index < 100; index++) {
     const new_cell = template.content.firstElementChild.cloneNode(true);
-    template.parentElement.append(new_cell);
+    board.append(new_cell);
 }
