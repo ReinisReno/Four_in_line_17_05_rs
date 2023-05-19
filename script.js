9570f604c45e0c434efe9edcb33b3b637d6028be
ReinisReno <reinisreno@gmail.com>
2023-05-19 18:59:34 +0300

    xuno darīts

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const template = document.querySelector('#game_cell');
 
 for (let index = 0; index < 100; index++) {
     const new_cell = template.content.firstElementChild.cloneNode(true);
+    new_cell.dataset.id = index;
     board.append(new_cell);
+    new_cell.addEventListener('click', clickHandle);
+}
+
+let count = 0;
+function clickHandle() {
+    if (this.textContent !== '') {
+        return;
+    }
+    const symbol = (++count % 2 == 0) ? 'o' : 'x';
+    this.textContent = symbol;
 }
