1497a8888651b21184b2b588e7e3e6dc5e9abf1e
ReinisReno <reinisreno@gmail.com>
2023-05-19 21:06:01 +0300

    Pielikām Referee

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const board = document.querySelector('.game_board');
 const template = document.querySelector('#game_cell');
 let count = 0;
 let referee = new Referee();
+let moves = {};
 
 for (let index = 0; index < 100; index++) {
     const new_cell = template.content.firstElementChild.cloneNode(true);
@@ -11,18 +12,19 @@ for (let index = 0; index < 100; index++) {
 }
 
 function clickHandle() {
-    if (this.textContent !== '') {
+    const id = Number(this.dataset.id);
+    if (moves.hasOwnProperty(id)) {
         return;
     }
-    const id = Number(this.dataset.id);
-    if (id < 90 && board.children[id + 10].textContent == "") {
+    if (id < 90 && !moves.hasOwnProperty(id + 10)) {
         return;
     }
 
     const symbol = (++count % 2 == 0) ? 'o' : 'x';
+    moves[id] = symbol;
     this.textContent = symbol;
 
-    if (referee.checkWinner(moves, symbol)) {
+    if (referee.checkWinner(moves, symbol, id)) {
         console.log("Player " + symbol + ' has won the game!');
     }
 }
