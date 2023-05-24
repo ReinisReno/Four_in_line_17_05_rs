bec0952db51e1a3b6a024741d2b278ec19b36ba7
ReinisReno <reinisreno@gmail.com>
2023-05-24 20:56:17 +0300

    Done_winners

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 const board = document.querySelector('.game_board');
 const template = document.querySelector('#game_cell');
 let count = 0;
-let referee = new Referee();
+let size = 10;
+let referee = new Referee(size);
 let moves = {};
 
-for (let index = 0; index < 100; index++) {
-    const new_cell = template.content.firstElementChild.cloneNode(true);
-    new_cell.dataset.id = index;
-    board.append(new_cell);
-    new_cell.addEventListener('click', clickHandle);
+for (let index = 0; index < size*size; index++) {
+  const new_cell = template.content.firstElementChild.cloneNode(true);
+  new_cell.dataset.id = index;
+  board.append(new_cell);
+  new_cell.addEventListener('click', clickHandle);
 }
 
-function clickHandle() {
-    const id = Number(this.dataset.id);
-    if (moves.hasOwnProperty(id)) {
-        return;
-    }
-    if (id < 90 && !moves.hasOwnProperty(id + 10)) {
-        return;
-    }
+function clickHandle () {
+  const id = Number(this.dataset.id);
+  if (moves.hasOwnProperty(id)) {
+    return;
+  }
 
-    const symbol = (++count % 2 == 0) ? 'o' : 'x';
-    moves[id] = symbol;
-    this.textContent = symbol;
+  if (id < (size - 1) * size && !moves.hasOwnProperty(id + size)) {
+    return ;
+  }
 
-    if (referee.checkWinner(moves, symbol, id)) {
-        console.log("Player " + symbol + ' has won the game!');
-    }
+  const symbol = (++count % 2 == 0) ? 'o' : 'x';
+  moves[id] = symbol;
+  this.textContent = symbol;
+
+  if (referee.checkWinner(moves, id)) {
+    console.log("Player " + symbol + ' has won the game!');
+  }
 }
+
